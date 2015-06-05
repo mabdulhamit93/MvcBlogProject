f831b0d4f93f0f1331e642292ee95662c8b0604c
mabdulhamit93 <m.abdulhamit93@hotmail.com>
2015-06-05 21:42:33 +0300

    Mvc Blog Project completed
    I can not show image. Last 10 post showed in main page with author date
    and detail.

---
+++ project/project/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using project.Models;
+using PagedList;
+
+namespace project.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private BlogDBContext db = new BlogDBContext();
+        
+        public ActionResult Index(int page=1)
+        {
+            int length = db.Blogs.Count();
+            List<Blog> article_list = db.Blogs.OrderByDescending(i => i.ReleaseDate).Take(length).ToList();
+            return View(article_list.OrderByDescending(x=>x.ReleaseDate).ToPagedList(page,10));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        public ActionResult Details(int id = 0)
+        {
+            Blog article = db.Blogs.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ project/project/Models/Blog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace project.Models
+{
+    public class Blog
+    {
+        public Blog()
+        {
+            ReleaseDate = DateTime.Now;
+        }
+        
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Content { get; set; }
+        public string Autor { get; set; }
+        
+    }
+
+    public class BlogDBContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
